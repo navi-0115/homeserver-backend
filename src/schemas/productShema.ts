cc1d612345f86763e2ee8eb16ecee20b7e6833e2
Raden-nvii <navinugraha15@gmail.com>
2024-09-22 23:41:59 +0800

    feat: add product schema zod

---
+++ src/schemas/productShema.ts
@@ -0,0 +1,56 @@
+// products/schema.ts
+import { z } from "@hono/zod-openapi";
+
+//Creating product
+export const createProductSchema = z.object({
+  name: z
+    .string()
+    .min(6, "Name is required")
+    .openapi({ example: "Rasberry Pi 5" }),
+  price: z
+    .number()
+    .min(0, "Price must be greater than 0")
+    .openapi({ example: 120000 }),
+  description: z.string().optional(),
+  image: z.string().url().optional().openapi({
+    example: "https://studio.youtube.com/channel/UCp_B66Jg1nCh04bdciEaXJg",
+  }),
+  stock: z
+    .number()
+    .min(0, "Stock must be greater than 0")
+    .openapi({ example: 5 }),
+});
+
+//Updating product
+export const updateProductSchema = z.object({
+  name: z
+    .string()
+    .min(6, "Name is required")
+    .openapi({ example: "Rasberry Pi 5" }),
+  price: z
+    .number()
+    .min(0, "Price must be greater than 0")
+    .openapi({ example: 120000 }),
+  description: z.string().optional(),
+  image: z.string().url().optional().openapi({
+    example: "https://studio.youtube.com/channel/UCp_B66Jg1nCh04bdciEaXJg",
+  }),
+  stock: z
+    .number()
+    .min(0, "Stock must be greater than 0")
+    .openapi({ example: 5 }),
+});
+
+// for product id schema
+export const productIdSchema = z.object({
+  id: z.coerce.number().int().min(1),
+});
+
+// for product slug schema
+export const productSlugSchema = z.object({
+  slug: z
+    .string()
+    .min(5)
+    .regex(/^[a-z0-9-]+$/, { message: "Invalid slug!" })
+    .openapi({ example: "raspberry-pi-5" }),
+});
