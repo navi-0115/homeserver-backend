e097d8e788cedb47f528581436b78404c3d5c0f8
Raden-nvii <navinugraha15@gmail.com>
2024-10-03 15:47:24 +0800

    feat(authschema): comment confirm password

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -11,22 +11,9 @@ export const loginSchema = z.object({
   }),
 });
 
-export const registerSchema = loginSchema
-  .extend({
-    name: z.string().min(1, "Name is required").max(255).openapi({
-      description: "The name of the user.",
-      example: "User",
-    }),
-    confirmPassword: z
-      .string()
-      .min(1, "Password confirmation is required")
-      .max(255)
-      .openapi({
-        description: "The password confirmation of the user.",
-        example: "secret",
-      }),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"],
-  });
+export const registerSchema = loginSchema.extend({
+  name: z.string().min(1, "Name is required").max(255).openapi({
+    description: "The name of the user.",
+    example: "User",
+  }),
+});
